6920536eda9f97a76ea3e3c7406fa23f23a3058a
alexandre-nestoridis-vinci <alexandre.nestoridis@student.vinci.be>
2025-06-19 23:03:45 +0200

    iteration 2: conteneurisation

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -22,14 +22,19 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
+  reporter: [
+    ['html', { open: 'never' }],
+],  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL:
+      process.env.NODE_ENV === undefined ||
+      process.env.NODE_ENV === "development"
+        ? "http://localhost:5173"
+        : "http://reverse-proxy:80",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
