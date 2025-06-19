f3c97fde0dde72242e7069b42f121e79d29f3445
alexandre-nestoridis-vinci <alexandre.nestoridis@student.vinci.be>
2025-06-19 22:25:58 +0200

    e2e

--- api/src/main/resources/data.sql
+++ api/src/main/resources/data.sql
@@ -1,1 +1,4 @@
-INSERT INTO users(username, password) VALUES('admin', '$2y$10$EW75BdcbCfKEA3NbE7in4OiYYacfP3OY4Q2JsVzjxd3jNxxgAEZNu');
+TRUNCATE TABLE users;
+
+INSERT INTO users(username, password)
+VALUES ('admin', '$2y$10$EW75BdcbCfKEA3NbE7in4OiYYacfP3OY4Q2JsVzjxd3jNxxgAEZNu');
